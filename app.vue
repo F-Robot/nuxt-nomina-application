69a00ecd4f42aee000165384c0855d653501bf37
Ricardo Blanco <rblanco@outlook.com>
2022-10-17 19:30:04 -0500

    feat: hide sidebar in mobile viewports

--- app.vue
+++ app.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
+import { useDisplay } from 'vuetify/lib/framework.mjs'
 import TheNavbar from '~/components/TheNavbar.vue'
 import TheSidebar from '~/components/TheSidebar.vue'
+const { mobile } = useDisplay()
 </script>
 
 <template>
   <VApp>
     <TheNavbar />
-    <TheSidebar />
+    <TheSidebar v-if="!mobile" />
     <VMain>
       <NuxtPage />
     </VMain>
